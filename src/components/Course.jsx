42445e8a7f53010694156a01323656a90b0aa9ed
TheDP66 <dharma66putra@gmail.com>
2022-07-15 21:33:51 +0700

    ....

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import TImage from "../images/latihan/tinder clone.png";
+import SSMImage from "../images/latihan/simple social media.png";
 import WAImage from "../images/latihan/whatsapp clone.png";
 import NFTMImage from "../images/latihan/nft marketplace.png";
+import ThreeImage from "../images/latihan/three course.png";
 
 const Course = () => {
   return (
@@ -58,6 +60,7 @@ const Course = () => {
               </div>
             </div>
           </div>
+
           <div className="w-full px-4 lg:w-1/2 xl:w-1/3">
             <div className="bg-white rounded-xl shadow-lg hover:shadow-xl overflow-hidden mb-10">
               <img src={TImage} alt="Programming" className="w-full" />
@@ -92,6 +95,42 @@ const Course = () => {
               </div>
             </div>
           </div>
+
+          <div className="w-full px-4 lg:w-1/2 xl:w-1/3">
+            <div className="bg-white rounded-xl shadow-lg hover:shadow-xl overflow-hidden mb-10">
+              <img src={SSMImage} alt="Programming" className="w-full" />
+              <div className="py-8 px-6">
+                <h3>
+                  <a
+                    href="https://62d17814a9f061271077c569--startling-cupcake-8ee2c7.netlify.app"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="blcok mb-3 font-semibold text-xl text-dark hover:text-primary truncate"
+                  >
+                    Simple Social Media
+                  </a>
+                </h3>
+                <p className="font-medium text-base text-secondary mb-6">
+                  Frontend : <b>React</b>
+                  <br />
+                  Backend : <b>Node JS</b>
+                  <br />
+                  Database : <b>MySQL</b>
+                  <br />
+                  Hosting : <b>Netlify</b> and <b>Heroku</b>
+                </p>
+                <a
+                  href="https://62d17814a9f061271077c569--startling-cupcake-8ee2c7.netlify.app"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="font-medium text-sm text-white bg-primary py-3 px-6 rounded-md hover:opacity-80"
+                >
+                  Explore Site
+                </a>
+              </div>
+            </div>
+          </div>
+
           <div className="w-full px-4 lg:w-1/2 xl:w-1/3">
             <div className="bg-white rounded-xl shadow-lg hover:shadow-xl overflow-hidden mb-10">
               <img src={NFTMImage} alt="Programming" className="w-full" />
@@ -119,6 +158,39 @@ const Course = () => {
                 </p>
                 <a
                   href="https://nft-marketplace-31e8e.web.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="font-medium text-sm text-white bg-primary py-3 px-6 rounded-md hover:opacity-80"
+                >
+                  Explore Site
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full px-4 lg:w-1/2 xl:w-1/3">
+            <div className="bg-white rounded-xl shadow-lg hover:shadow-xl overflow-hidden mb-10">
+              <img src={ThreeImage} alt="Programming" className="w-full" />
+              <div className="py-8 px-6">
+                <h3>
+                  <a
+                    href="https://three-course.web.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="blcok mb-3 font-semibold text-xl text-dark hover:text-primary truncate"
+                  >
+                    Three Course
+                  </a>
+                </h3>
+                <p className="font-medium text-base text-secondary mb-6">
+                  Frontend : <b>React JS</b>
+                  <br />
+                  Hosting : <b>Firebase</b>
+                  <br />
+                  3rd Party : <b>Three</b>, <b>Drei</b>, and <b>Fiber</b>
+                </p>
+                <a
+                  href="https://three-course.web.app/"
                   target="_blank"
                   rel="noreferrer"
                   className="font-medium text-sm text-white bg-primary py-3 px-6 rounded-md hover:opacity-80"
